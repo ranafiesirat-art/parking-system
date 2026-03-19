6358b1496a735c2eb17c325581247b659c9aa9de
ranafie <ranafie.sirat@gmail.com>
2026-03-20 01:36:41 +0800

    auto update

--- mileage_redirect.php
+++ mileage_redirect.php
@@ -39,5 +39,5 @@ $token = base64_encode($username . '|' . $timestamp . '|' . $hash);
 | Redirect ke Mileage System (ROOT SAFE REDIRECT)
 |--------------------------------------------------------------------------
 */
-header("Location: http://localhost/mileage-system/login.php?token=" . urlencode($token));
+header("Location: https://mileage.nrinnovations.my/login.php?token=" . urlencode($token));
 exit;
